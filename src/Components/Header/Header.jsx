09a56bd6e42ef484b08cb279c9c022810b6efd07
Harsh <harshjat12345671@gmail.com>
2024-11-02 15:44:23 +0530

    create header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
+import { icons } from "../../assets/assets";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   return (
-    <div>Hello</div>
-  )
-}
+    <header className="header">
+      <div className="logo">Harsh</div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li>
+            <img src={icons.home} alt="home icon" />
+            <span>Home</span>
+          </li>
+          <li>
+            <img src={icons.about} alt="about icon" />
+            <span>About</span>
+          </li>
+          <li>
+            <img src={icons.project} alt="project icon" />
+            <span>Project</span>
+          </li>
+          <li>
+            <img src={icons.resume} alt="resume icon" />
+            <span>Resume</span>
+          </li>
+          {/* Dropdown Menu */}
+          <li className="nav-item dropdown">
+            <img src={icons.other} alt="other icon" />
+            <button className="dropdown-button">
+              Others ▼
+            </button>
+            <div className="dropdown-content">
+              <a href="#option1"><img src={icons.skill} alt="skill icon" />Skills</a>
+              <a href="#option2"><img src={icons.certificate} alt="certificate icon" />Certificates</a>
+            </div>
+          </li>
+          <li>
+            <img src={icons.contact} alt="contact icon" />
+            <span>Contact</span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/assets/assets.js
@@ -0,0 +1,24 @@
+//icons   
+import home from './images/icons8-home-32.png';
+import about from './images/icons8-person-64.png'
+import project from './images/icons8-project-50.png'
+import other from './images/icons8-other-24.png'
+import certificate from './images/icons8-certificate-80.png'
+import resume from './images/icons8-resume-50.png'
+import skill from './images/icons8-skill-50.png'
+import contact from './images/icons8-phone-50.png'
+import git from './images/icons8-git-50.png'
+import star from './images/icons8-star-50.png'
+ 
+export const icons = {
+    home,
+    about,
+    project,
+    other,
+    certificate,
+    resume,
+    skill,
+    contact,
+    git,
+    star
+};
